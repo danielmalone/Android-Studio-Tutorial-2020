5b873b4a885a577938fc9ed3ed728d08b7471d9d
Daniel Malone <danieljmalone@gmail.com>
2020-03-07 11:32:40 -0600

    Finish video #4

--- app/src/main/java/com/example/AddProductActivity.kt
+++ app/src/main/java/com/example/AddProductActivity.kt
@@ -1,12 +1,24 @@
 package com.example
 
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.add_product.*
 
-class AddProductActivity: AppCompatActivity() {
+class AddProductActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_product)
+
+        addProductSubmitButton.setOnClickListener {
+
+            val database = getSharedPreferences("database", Context.MODE_PRIVATE)
+            database.edit().apply {
+                putString("savedProductName", editTextProductName.text.toString())
+            }.apply()
+
+        }
+
     }
 }
--- app/src/main/java/com/example/MainActivity.kt
+++ app/src/main/java/com/example/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log.d
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
@@ -13,10 +15,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-
         goToAddProduct.setOnClickListener {
             startActivity(Intent(this, AddProductActivity::class.java))
         }
+
+        val preferences = getSharedPreferences("database", Context.MODE_PRIVATE)
+        val savedName = preferences.getString("savedProductName", "This value doesn't exist.")
+        d("daniel", "saved message is: $savedName")
+
+        lastSavedProduct.text = savedName
 
     }
 }
